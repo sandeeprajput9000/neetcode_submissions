96064124cc2ea4414888e6ab1e1321bdf1dc2107
sandeep rajput <sandeepnit9328@gmail.com>
2026-08-07 22:21:52 +0530

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+
+// APPROACH 1
+
+    // int longestConsecutive(vector<int>& nums) {
+    //     int maxi=INT_MIN;
+    //     int n=nums.size();
+        // if(n==0) return 0;
+        // unordered_map<int,int>mp;
+        // for(int i=0;i<n;i++)
+        //   mp[nums[i]]++;
+
+    //     for(int i=0;i<n;i++){
+    //         if(mp.find(nums[i]-1) == mp.end()) continue;
+    //         int temp=nums[i];
+    //         int cnt=1;
+    //         while(mp.find(temp+1)!=mp.end()){
+    //             cnt++;
+    //             temp=temp+1;
+    //         }
+    //         maxi=max(maxi,cnt);
+    //     }
+    //     return maxi;
+    // }
+
+
+     int longestConsecutive(vector<int>& nums) {
+            vector<int>start;
+            int maxi=INT_MIN;
+            int n=nums.size();
+            if(n==0) return 0;
+            unordered_map<int,int>mp;
+            for(int i=0;i<n;i++)
+               mp[nums[i]]++;
+
+             for(int i=0;i<nums.size();i++){
+                if(mp.find(nums[i]-1)==mp.end()){
+                   int temp=nums[i];
+                    int cnt=1;
+                    while(mp.find(temp+1)!=mp.end()){
+                        cnt++;
+                        temp=temp+1;
+                    }
+                maxi=max(maxi,cnt);
+             }
+          }
+         return maxi;
+     }
+};
